c68774ac2ec8ec95b938c8cc452fa039dbb1b5c2
Denio Nhanale <dnhanale@gmail.com>
2023-06-20 13:11:40 +0200

    Fetch missions from api

--- src/redux/missions/MissionSlice.js
+++ src/redux/missions/MissionSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const missionUrl = 'https://api.spacexdata.com/v3/missions';
+
+const getMissions = createAsyncThunk(
+  'missions/fecth-missions',
+  async (thunkAPI) => {
+    try {
+      const response = await fetch(missionUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Try Again!');
+    }
+  },
+);
 
 const initialState = {
   missions: [],
@@ -7,6 +22,19 @@ const initialState = {
 const MissionSlice = createSlice({
   name: 'mission',
   initialState,
+  extraReducers: (builder) => {
+    builder.addCase(GetMissions.fulfilled, (state, action) => {
+      const set = action.payload.map((mission) => ({
+        ...mission,
+        member: false,
+      }));
+      return {
+        ...state,
+        missions: set,
+      };
+    });
+  },
 });
 
+export { getMissions };
 export default MissionSlice.reducer;
