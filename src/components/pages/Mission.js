3eef84347a0fc1763522748880615aff31cd5aca
Denio Nhanale <dnhanale@gmail.com>
2023-06-20 10:04:29 +0200

    Create a MissionSlice

---
+++ src/components/pages/Mission.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Mission = () => (
+  <div>Missions</div>
+);
+
+export default Mission;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -7,7 +8,12 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+
 );
