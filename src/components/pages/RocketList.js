49f3958aa9a965d752c3eb1f9e650b91468eb8d6
WilhelmK109 <kamulungaw@gmail.com>
2023-06-21 18:02:03 +0200

    Implement the rocket booking and cancellation actions

--- src/components/pages/RocketList.js
+++ src/components/pages/RocketList.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket, cancelRocket } from '../../redux/rockets/rocketsSlice';
 
 export default function RocketList({ rocket = {} }) {
+  const dispatch = useDispatch();
+
+  const handleReserveButtonClick = () => {
+    dispatch(reserveRocket(rocket.id));
+  };
+
+  const handleCancelReservationButtonClick = () => {
+    dispatch(cancelRocket(rocket.id));
+  };
+
   return (
     <div className="rocket-list-item">
       <img className="rocket-image" src={rocket.flickr_images} alt={rocket.name} />
       <div className="rocket-info">
         <h2>{rocket.rocket_name}</h2>
-        {rocket.description}
+        <p>
+          {rocket.reserved && <span className="rocket-badge">Reserved</span>}
+          {' '}
+          {rocket.description}
+        </p>
+        {rocket.reserved && <button className="cancel-btn" type="button" onClick={handleCancelReservationButtonClick}>Cancel reservation</button>}
+        {!rocket.reserved && <button className="rocketreserved-btn" type="button" onClick={handleReserveButtonClick}>Reserve rocket</button>}
       </div>
     </div>
   );
@@ -19,5 +37,6 @@ RocketList.propTypes = {
     rocket_name: PropTypes.string,
     description: PropTypes.string,
     flickr_images: PropTypes.string,
+    reserved: PropTypes.bool,
   }).isRequired,
 };
