e8654de0b24cf7db08fd36a5bf4bef60db59adca
WilhelmK109 <kamulungaw@gmail.com>
2023-06-21 10:53:07 +0200

    Add code to fetch data from rockets endpoint

--- src/components/pages/Rocket.js
+++ src/components/pages/Rocket.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import RocketList from './RocketList';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
-const Rocket = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const { rockets, isFetched } = useSelector((state) => state.rockets);
 
-export default Rocket;
+  useEffect(() => {
+    if (!isFetched) {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, isFetched]);
+
+  return (
+    <div>
+      <h1>Rockets</h1>
+      {rockets.map((rocket) => (
+        <RocketList rocket={rocket} key={rocket.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Rockets;
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,12 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   rockets: [],
+  status: 'idle',
+  isFetched: false,
+  error: null,
 };
 
-const rocketsSlice = createSlice({
+export const fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    try {
+      const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
+);
+
+export const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => ({ ...state, status: 'loading' }))
+      .addCase(fetchRockets.fulfilled, (state, action) => ({
+        ...state, status: 'succeeded', rockets: action.payload, isFetched: true,
+      }))
+      .addCase(fetchRockets.rejected, (state, action) => ({ ...state, status: 'failed', error: action.error.message }));
+  },
 });
 
 export default rocketsSlice.reducer;
