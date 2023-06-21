e6bea99fbbd39540ab77c559a1ba3db7497c948d
Denio Nhanale <dnhanale@gmail.com>
2023-06-21 13:56:04 +0200

    Render Missions

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,16 +1,46 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getMissions } from '../../redux/mission/MissionSlice';
 
 const Mission = () => {
   const dispatch = useDispatch();
+  const missions = useSelector((state) => state.mission.missions || []);
 
   useEffect(() => {
-    dispatch(getMissions());
-  }, [dispatch]);
+    if (!missions.length)dispatch(getMissions());
+  }, [dispatch, missions.length]);
 
   return (
-    <div>Missions</div>
+    <table className="mission-tb" cellSpacing={0}>
+      <thead>
+        <tr>
+          <th className="head">Mission</th>
+          <th className="head">Description</th>
+          <th className="head">Status</th>
+          <th className="head">Join</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          missions.map((mission) => (
+            <tr className="mission-id" key={mission.mission_id}>
+              <td className="mission-name">{mission.mission_name}</td>
+              <td className="mission-description">{mission.description}</td>
+              <td>
+                <span className="mission-status">
+                  Not a member
+                </span>
+              </td>
+              <td>
+                <button type="button" className="join-mission">
+                  Join-Mission
+                </button>
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   );
 };
 
