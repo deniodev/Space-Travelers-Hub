981340a4c2bbc95a15a7c55443482c9d8f504701
Denio Nhanale <dnhanale@gmail.com>
2023-06-22 14:24:56 +0200

    Display Joined Missions in My Profile Page

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 const Profile = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const allMissions = useSelector((state) => state.mission);
+  const joinedMissions = allMissions.missions.filter((mymission) => mymission.member);
   return (
     <div className="profile-content">
-      <div className="missions">
-        <h2>My Missions</h2>
+      <div className="m-joined">
+        <h1>My Missions</h1>
+        {
+            joinedMissions.length ? (
+              <ul className="m-joined-profile">
+                {
+                  joinedMissions.map((mission) => (
+                    <li key={mission.mission_id}>
+                      {mission.mission_name}
+                    </li>
+                  ))
+                }
+              </ul>
+            ) : (
+              <div>
+                <p>No missions joined yet!</p>
+                <span>
+                  Go to
+                  <NavLink to="/missions"> Missions </NavLink>
+                  to join a mission
+                </span>
+              </div>
+            )
+          }
       </div>
 
       <div className="rockets">
